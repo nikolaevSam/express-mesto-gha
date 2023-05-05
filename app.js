17d6bd4e0b0a4ae47c2908fb07321011a9697c39
nikolaevSam <nikolaev.promobogrev@yandex.ru>
2023-05-05 12:49:13 +0300

    test done#2

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 const bodyParser = require('body-parser');
 
-const {
-  PORT = 3000,
-  URL = 'mongodb://localhost:27017/mestodb'
-} = process.env;
+const { PORT = 3000 } = process.env;
+const URL = 'mongodb://localhost:27017/mestodb'
 
-const app = express();
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+});
 
 app.use((req, res, next) => {
   req.user = {
@@ -15,11 +20,6 @@ app.use((req, res, next) => {
   };
   next();
 });
-
-app.use(bodyParser.json());
-
-mongoose.connect(URL);
-
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,5 @@ router.get('/users/:userId', getUserById);
 router.post('/users', createUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
-router.use('/*', (request, response) => {
-  response.status(404)
-    .send({ message: '404: Not Found' });
-});
 
 module.exports = router;
